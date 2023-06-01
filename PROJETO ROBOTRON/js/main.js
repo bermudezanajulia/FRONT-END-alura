db24c3fdf7d063801029325eb6f12df35bf4c05e
bermudezanajulia <anajuliabermudez321@gmail.com>
2023-05-31 22:42:07 -0300

    .

--- PROJETO ROBOTRON/js/main.js
+++ PROJETO ROBOTRON/js/main.js
@@ -1,8 +1,46 @@
 const controle = document.querySelectorAll("[data-controle]")
+const estatistica= document.querySelectorAll("[data-estatistica]")
+
+console.log(estatistica)
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
 controle.forEach( (elemento) => {
 elemento.addEventListener("click", (evento)=> {
     manipulaDados(evento.target.textContent, evento.target.parentNode)
+    atualizaEstatisticas(evento.target.dataset.peca)
     })
 })
     
@@ -15,3 +53,10 @@ function manipulaDados(operacao, controle) {
         peca.value = parseInt(peca.value) + 1
     }
 }
+
+function atualizaEstatisticas (peca) {
+    console.log(peca[peca])}
+
+    controle.forEach(  (elemento) => {
+        console.log(elemento.textContent)
+    } )
