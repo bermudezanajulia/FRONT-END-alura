faf89594af09255c44c9e01d059c3a7fb9c44948
bermudezanajulia <anajuliabermudez321@gmail.com>
2023-06-12 11:45:47 -0300

    novo estudo de objetos

---
+++ JAVA SCRIPT/MANIPULANDO_OBJETOS/main.js
@@ -0,0 +1,14 @@
+ 
+
+let cliente = {
+    nome: 'Mariana' ,
+    idade: 35,
+    genero: 'Feminino'}
+
+    cliente['estado'] = 'Pernanbuco';
+    
+        alert('Olá meu nome é ' + cliente.nome + " e moro no estado de " + cliente.estado);
+    
+    
+    
+    
